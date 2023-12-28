40489edd0f76c82911551d8e58e7628e82b24a68
Jerry Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2023-12-28 16:38:34 -0500

    Try to fix autoscaling 10

--- main.tf
+++ main.tf
@@ -85,12 +85,12 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      # targets = {
+      #  my_target = {
+      #    target_id = aws_instance.blog.id
+      #    port = 80
+      #  }
+      # }
     }
   }
 

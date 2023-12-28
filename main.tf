015d5fc826fd5e3a0cb2a84dfa274c7c05621cb5
Jerry Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2023-12-28 15:04:36 -0500

    Try to fix LB 4

--- main.tf
+++ main.tf
@@ -103,15 +103,11 @@ module "alb" {
   }
 
   target_groups = {
-    name_prefix      = "blog"
-    backend_protocol = "HTTP"
-    backend_port     = 80
-    target_type      = "instance"
-    targets = {
-      my_target = {
-        target_id = aws_instance.blog.id
-        port = 80
-      }
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
     }
   }
 

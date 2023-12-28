f56f16be321965201805163a0f9e0d5750975523
Jerry Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2023-12-28 16:59:26 -0500

    Try to fix LoadBalancer 6

--- main.tf
+++ main.tf
@@ -64,64 +64,36 @@ module "autoscaling" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
+  target_groups = [
+    {
+      ex-instance = {
+        name_prefix      = "blog-"
+        protocol         = "HTTP"
+        port             = 80
+        target_type      = "instance"
+        targets = {
+          my_target = {
+            target_id = aws_instance.blog.id
+            port = 80
+          }
+        }
+      }
     }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
-  }
+  ]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "h1"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-    }
-  }
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
+  http_tcp_listeners = [
+    {
+      port = 80
       protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
+      target_group_index = 0
     }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
+  ]
 
   tags = {
     Environment = "dev"

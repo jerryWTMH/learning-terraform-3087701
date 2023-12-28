f3b482e6cd347d0acc8850d79cb264f3042dbc27
Jerry Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2023-12-28 16:41:40 -0500

    Try to fix autoscaling 11

--- main.tf
+++ main.tf
@@ -46,7 +46,21 @@ resource "aws_instance" "blog" {
 }
 
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  # target_group_arns   = [for group in module.blog_alb.target_groups : group.arn]
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+}
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
@@ -85,12 +99,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      # targets = {
-      #  my_target = {
-      #    target_id = aws_instance.blog.id
-      #    port = 80
-      #  }
-      # }
     }
   }
 
